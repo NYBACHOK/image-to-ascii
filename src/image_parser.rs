a2a9b20f7cfdbc6c434491624ca5d649fc744acf
NYBACHOK <nybachokua@gmail.com>
2023-06-15 18:26:13 +0300

    Remove image to other mode

---
+++ src/image_parser.rs
@@ -0,0 +1,66 @@
+use eyre::{Result, WrapErr};
+use image::imageops::FilterType;
+use image::io::Reader as ImageReader;
+use image::GenericImageView;
+use std::fs::File;
+use std::io::Write;
+
+const WIDTH_SCALE: u32 = 2;
+
+pub fn parse_image(path: &str, output: Option<String>) -> Result<()> {
+    let mut img = ImageReader::open(path)
+        .wrap_err("Failed to open image")?
+        .decode()
+        .unwrap_or_else(|_| {
+            eprintln!("Failed to decode image");
+            std::process::exit(2)
+        })
+        .grayscale();
+
+    let (width, height) = match output {
+        None => {
+            let (terminal_width, terminal_height) = termion::terminal_size().unwrap();
+            let scale = std::cmp::max(
+                img.width() * WIDTH_SCALE / (terminal_width as u32 - 1),
+                img.height() / terminal_height as u32,
+            );
+
+            ((img.width() / scale * WIDTH_SCALE), img.height() / scale)
+        }
+        Some(_) => (img.width(), img.height()),
+    };
+
+    img = img.resize(width, height, FilterType::Nearest);
+
+    const CHARACTER_SET: [&str; 11] = ["@", "#", "0", "O", "L", ";", ":", ".", ",", "'", " "];
+
+    let mut art = String::with_capacity((img.height() * img.width()) as usize);
+    let mut last_y = 0;
+    for pixel in img.pixels() {
+        if last_y != pixel.1 {
+            art.push_str("\n");
+            last_y = pixel.1;
+        }
+
+        let pixel_data = pixel.2;
+        let brightness: f64 =
+            ((pixel_data[0] as u64 + pixel_data[1] as u64 + pixel_data[2] as u64) / 3) as f64;
+        let character_position =
+            ((brightness / 255.0) * (CHARACTER_SET.len() - 1) as f64).round() as usize;
+        art.push_str(CHARACTER_SET[character_position])
+    }
+
+    match output {
+        None => {
+            println!("{}", art);
+        }
+        Some(path) => {
+            let mut file = File::create(path).wrap_err("Failed to create file")?;
+
+            file.flush().unwrap();
+            file.write_all(art.as_bytes()).unwrap();
+        }
+    };
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use clap::Parser;
-use eyre::{Result, WrapErr};
-use image::imageops::FilterType;
-use image::io::Reader as ImageReader;
-use image::GenericImageView;
-use std::fs::File;
-use std::io::Write;
+mod image_parser;
+mod video_parser;
+
+use clap::{Parser, ValueEnum};
+use eyre::{Result};
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum FileType {
+    Image,
+    Video,
+}
 
 #[derive(Parser, Debug)]
 #[command(version)]
 struct Args {
+    #[arg(short, long)]
+    file_type: FileType,
+
     #[arg(short, long)]
     path: String,
 
@@ -16,64 +23,15 @@ struct Args {
     output: Option<String>,
 }
 
-const WIDTH_SCALE: u32 = 2;
-
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let mut img = ImageReader::open(args.path)
-        .wrap_err("Failed to open image")?
-        .decode()
-        .unwrap_or_else(|_| {
-            eprintln!("Failed to decode image");
-            std::process::exit(2)
-        })
-        .grayscale();
-
-    let (width, height) = match args.output {
-        None => {
-            let (terminal_width, terminal_height) = termion::terminal_size().unwrap();
-            let scale = std::cmp::max(
-                img.width() * WIDTH_SCALE / (terminal_width as u32 - 1),
-                img.height() / terminal_height as u32,
-            );
-
-            ((img.width() / scale * WIDTH_SCALE), img.height() / scale)
+    match args.file_type {
+        FileType::Image => {
+            image_parser::parse_image(args.path.as_str(), args.output)?;
         }
-        Some(_) => (img.width(), img.height()),
-    };
-
-    img = img.resize(width, height, FilterType::Nearest);
-
-    const CHARACTER_SET: [&str; 11] = ["@", "#", "0", "O", "L", ";", ":", ".", ",", "'", " "];
-
-    let mut art = String::with_capacity((img.height() * img.width()) as usize);
-    let mut last_y = 0;
-    for pixel in img.pixels() {
-        if last_y != pixel.1 {
-            art.push_str("\n");
-            last_y = pixel.1;
-        }
-
-        let pixel_data = pixel.2;
-        let brightness: f64 =
-            ((pixel_data[0] as u64 + pixel_data[1] as u64 + pixel_data[2] as u64) / 3) as f64;
-        let character_position =
-            ((brightness / 255.0) * (CHARACTER_SET.len() - 1) as f64).round() as usize;
-        art.push_str(CHARACTER_SET[character_position])
+        FileType::Video => {}
     }
-
-    match args.output {
-        None => {
-            println!("{}", art);
-        }
-        Some(path) => {
-            let mut file = File::create(path).wrap_err("Failed to create file")?;
-
-            file.flush().unwrap();
-            file.write_all(art.as_bytes()).unwrap();
-        }
-    };
 
     Ok(())
 }
